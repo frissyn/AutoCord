2e6afe2b2c567b4ebd3f78fb1491a9721f9aa105
Name?= null <Ire.Raufu76@gmail.com>
2021-01-01 18:57:05 +0000

    update search function at library page

--- web/static/js/search.js
+++ web/static/js/search.js
@@ -1,3 +1,37 @@
+function sortByABC() {
+    var table, rows, switching, shouldSwitch;
+
+    switching = true;
+    table = document.getElementById("bot-table");
+
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+
+        for (var i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+
+            var x = rows[i].getElementsByTagName("td")[0];
+            var y = rows[i + 1].getElementsByTagName("td")[0];
+
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                shouldSwitch = true; break;
+            }
+        }
+
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
+
+
+function sortByStatus() {
+    alert("Not Yet Implemented.");
+}
+
+
 function search() {
     var input, filter, table, text, tr, td;
     
