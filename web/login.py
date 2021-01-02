85a97901f2e7969007b5489fd846d9047e24bc48
Name?= null <Ire.Raufu76@gmail.com>
2021-01-02 22:13:51 +0000

    fix: make load_user a public function

--- web/login.py
+++ web/login.py
@@ -4,7 +4,7 @@ from web import login_manager
 
 
 @login_manager.user_loader
-def _load_user(user_id: int):
+def load_user(user_id: int):
     try:
         result = User.query.filter_by(id=user_id).first()
         return result
